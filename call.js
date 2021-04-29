c6bab8ae003d44b90a4cf1dc16bde352d036a0fa
wb-wangshuang <572644539@qq.com>
2021-04-29 23:37:06 +0800

    call

--- call.js
+++ call.js
@@ -16,3 +16,12 @@ shows = () => {
   return this;
 };
 shows();
+
+Function.prototype.myCall = function (context = window, ...args) {
+  //apply is args
+  const fn = Symbol("fn");
+  context[fn] = this;
+  const res = context[fn](...args);
+  delete context[fn];
+  return res;
+};
